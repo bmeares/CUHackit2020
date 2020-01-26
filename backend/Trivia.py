2ea52f9224a4a9de569d32c488c8048f4d0b16c8
Bennett Meares <bennett.meares@gmail.com>
2020-01-25 20:32:36 -0500

    commit

--- backend/Trivia.py
+++ backend/Trivia.py
@@ -1,14 +1,5 @@
-from flask import Flask, session, redirect, url_for, escape, request
 import pandas as pd
 import sqlalchemy 
-
-app = Flask(__name__)
-engine = sqlalchemy.create_engine('mysql://cuhackit2020:gurgle@inscribe.productions/hackbox_db')
-
-# Set the secret key to some random bytes. Keep this really secret!
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-
-return trivia.stages[trivia.currentState](stuff)
 
 class Game:
   def __init__(self, engine):
@@ -117,25 +108,3 @@ class Trivia(Game):
   def generateQuestions(self):
      return
 
-@app.route('/')
-def index():
-    if 'username' in session:
-        return 'Logged in as %s' % escape(session['username'])
-    return 'You are not logged in'
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-      pass
-    return '''
-        <form method="post">
-            <p><input type=text name=username>
-            <p><input type=submit value=Login>
-        </form>
-    '''
-
-@app.route('/logout')
-def logout():
-    # remove the username from the session if it's there
-    session.pop('username', None)
-    return redirect(url_for('index'))
